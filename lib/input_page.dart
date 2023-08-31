fed36072a3d43820e952b2f7c4cd0a1faa0e7b35
Rayan Bahadur <r.bahadur@se22.qmul.ac.uk>
2023-08-31 15:56:45 +0100

    Constants and cleaning up code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1e1f31);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -16,23 +20,29 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReuseableCard(colour:Color(0xFF1e1f31),)),
-              Expanded(child: ReuseableCard(colour:Color(0xFF1e1f31),)),
+              Expanded(child: ReuseableCard(colour: activeCardColour,)),
+              Expanded(child: ReuseableCard(colour: activeCardColour,)),
             ],
           )),
           Expanded(child: Container(
             margin: const EdgeInsets.all(15.0),
             decoration: BoxDecoration(
-              color: const Color(0xFF1e1f31),
+              color: activeCardColour,
               borderRadius: BorderRadius.circular(10.0),
             ),
           ),),
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReuseableCard(colour:Color(0xFF1e1f31),)),
-              Expanded(child: ReuseableCard(colour:Color(0xFF1e1f31),)),
+              Expanded(child: ReuseableCard(colour: activeCardColour,)),
+              Expanded(child: ReuseableCard(colour: activeCardColour,)),
             ],
           )),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
         ],
       ),
     );
